5b56197c46daa32ceec880ceb1f6ca99f7d62f57
EricBruch <eric.bruch@outlook.de>
2020-10-17 18:16:48 +0200

    Added files, added and integrated definition file

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -5,6 +5,7 @@ import {
   OnInit,
   ViewChild,
 } from '@angular/core';
+import { CHARACTER_STATUS, GAME_SPEED, JUMP_TIME } from './definition';
 
 @Component({
   selector: 'app-canvas',
@@ -12,8 +13,8 @@ import {
   styleUrls: ['./canvas.component.scss'],
 })
 export class CanvasComponent implements OnInit {
-  character_x: number = 0;
-  character_y: number = 425;
+  character_x: number = 100;
+  character_y: number = 260;
   bg_elements: number = 0;
   isMovingRight: boolean = false;
   isMovingLeft: boolean = false;
@@ -23,11 +24,9 @@ export class CanvasComponent implements OnInit {
   myCanvas: ElementRef<HTMLCanvasElement>;
   public context: CanvasRenderingContext2D;
   character_image = new Image();
-  background_image = new Image();
-
-  //################################# Game Config ##################################
-  JUMP_TIME = 300; // in ms
-
+  currentCharacterImage: string = 'assets/img/animation/idle/I-1.png';
+  background_image_1 = new Image();
+  background_image_2 = new Image();
 
   constructor() {}
 
@@ -36,19 +35,30 @@ export class CanvasComponent implements OnInit {
   ngAfterViewInit(): void {
     this.context = this.myCanvas.nativeElement.getContext('2d');
     this.loadResources();
+    this.checkForRunning();
     this.draw();
   }
 
   loadResources() {
-    this.character_image.src = 'assets/img/mexican.png';
-    this.background_image.src = 'assets/img/background_1.png';
+    this.background_image_1.src = 'assets/img/Completo.png';
+  }
+
+  checkForRunning() {
+    setInterval(() => {
+      if (this.isMovingRight) {
+        if (this.currentCharacterImage == 'assets/img/animation/idle/I-1.png') {
+          this.currentCharacterImage = 'assets/img/animation/walk/right/W-R-1.png';
+        } else {
+          this.currentCharacterImage = 'assets/img/animation/idle/I-1.png';
+        }
+      }
+    }, 200);
   }
 
   draw() {
     this.drawBackgroundPicture();
     this.updateCharacter();
     let drawFunction = () => this.draw();
-
     try {
       requestAnimationFrame(drawFunction);
     } catch (error) {
@@ -57,17 +67,18 @@ export class CanvasComponent implements OnInit {
   }
 
   updateCharacter() {
+    this.character_image.src = this.currentCharacterImage;
     let timePassedSinceJump = new Date().getTime() - this.lastJumpStarted;
 
-    if (timePassedSinceJump < this.JUMP_TIME) {
+    if (timePassedSinceJump < JUMP_TIME) {
       this.character_y -= 10;
     } else {
       // Check falling
-      if (this.character_y < 425) {
+      if (this.character_y < 260) {
         this.character_y += 10;
       }
     }
-    // draw picture
+    // draw character
     if (this.character_image.complete) {
       this.context.drawImage(
         this.character_image,
@@ -81,35 +92,58 @@ export class CanvasComponent implements OnInit {
 
   drawBackgroundPicture() {
     if (this.isMovingLeft) {
-      this.bg_elements += 2;
+      this.bg_elements += GAME_SPEED;
     }
     if (this.isMovingRight) {
-      this.bg_elements -= 2;
+      this.bg_elements -= GAME_SPEED;
     }
     let canvas = this.myCanvas.nativeElement;
-    if (this.background_image.complete) {
-      this.context.drawImage(
-        this.background_image,
-        this.bg_elements,
-        0,
-        canvas.width,
-        canvas.height
-      );
+    this.addBackgroundObject(
+      this.background_image_1,
+      0,
+      0,
+      canvas.width * 3,
+      canvas.height,
+      1,
+      1
+    );
+    this.addBackgroundObject(
+      this.background_image_1,
+      canvas.width * 3,
+      0,
+      canvas.width * 3,
+      canvas.height,
+      1,
+      1
+    );
+  }
+
+  addBackgroundObject(obj, offset_x, offset_y, width, height, scale, opacity) {
+    if (opacity != undefined) {
+      this.context.globalAlpha = opacity;
     }
+    this.context.drawImage(
+      obj,
+      offset_x + this.bg_elements,
+      offset_y,
+      width * scale,
+      height * scale
+    );
+    this.context.globalAlpha = 1;
   }
 
   @HostListener('document:keydown', ['$event'])
   onKeyDown(e: KeyboardEvent) {
     if (e.key === 'ArrowLeft') {
       this.isMovingLeft = true;
-      this.character_x -= 10;
+      // this.character_x -= 10;
     }
     if (e.key == 'ArrowRight') {
       this.isMovingRight = true;
-      this.character_x += 10;
+      // this.character_x += 10;
     }
     let timePassedSinceJump = new Date().getTime() - this.lastJumpStarted;
-    if (e.code == 'Space' && timePassedSinceJump > (this.JUMP_TIME * 2)) {
+    if (e.code == 'Space' && timePassedSinceJump > JUMP_TIME * 2) {
       this.lastJumpStarted = new Date().getTime();
     }
   }
